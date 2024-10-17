2a3e5e28661effd4d784d2c0bc17eddc97312c7d
AylinEstrada <paesbe18@gmail.com>
2024-10-17 10:46:58 -0600

    solucion de los ejercicios del 6 al 9

--- arreglos8.c
+++ arreglos8.c
@@ -7,9 +7,17 @@
 //  @param una matriz
 //  @param el número de filas
 
+int suma(int a[][3], int f);
 
 int main() {
     int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+     
+    int res[][3] = suma(arr, 3);
+    int res = suma(arr, 3);
+    int *ptr = &res;
+
+    printf("%d\t", *(ptr));
+
 
     // TODO: (23) Crea una variable llamada resultado  y asigna lo que devuelve la función suma
     // pasa a la función el arreglo y su tamaño
@@ -19,6 +27,17 @@ int main() {
     
     return 0;
 }
+
+int suma(int a[][3], int f){
+    int r = 0;
+    for (int i = 0; i < f; i++){
+        for (int j = 0; j < 3; j++){
+            r += a[i][j];
+        }
+        
+     }
+}
+    
 
 
 //TODO: (25) Crea la función suma que realiza la suma de todos los elementos de un arreglo
